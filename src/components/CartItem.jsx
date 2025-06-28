1ba8fb625dc85ffc443328872196a38de4f12f9e
Mahajan-Om <mahajanom635@gmail.com>
2025-06-28 15:48:44 +0530

    chnages made

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,7 +1,7 @@
 
 import {AiFillDelete} from "react-icons/ai"
 import { useDispatch } from "react-redux";
-import { remove } from "../redux/Slices/CartSlice";
+import { remove } from "../redux/slices/CartSlice";
 import { toast } from "react-hot-toast";
 
 const CartItem = ({item, itemIndex}) => {
